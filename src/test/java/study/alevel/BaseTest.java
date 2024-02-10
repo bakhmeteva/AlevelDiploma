47dd2d1ac70868650d15ded48e354cc51e592fa9
Bakhmeteva Yulya <bakhmetevayulya@gmail.com>
2024-02-10 17:04:10 +0200

    Еще попытка скрина

--- src/test/java/study/alevel/BaseTest.java
+++ src/test/java/study/alevel/BaseTest.java
@@ -1,11 +1,16 @@
 package study.alevel;
 
 import io.qameta.allure.Attachment;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestResult;
+import org.testng.Reporter;
 import org.testng.annotations.*;
 import study.alevel.core.BrowserManager;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTest {
 
@@ -28,13 +33,27 @@ public class BaseTest {
     public void tearDown(ITestResult result) {
         if (result.getStatus() == ITestResult.FAILURE) {
             // Сделать скриншот и сохранить его
-            takeScreenshot();
+            captureScreenshot(result);
         }
     }
 
-    @Attachment(value = "Page screenshot", type = "image/png")
-    public byte[] takeScreenshot() {
-        return ((TakesScreenshot) BrowserManager.getDriver()).getScreenshotAs(OutputType.BYTES);
+    public void captureScreenshot(ITestResult result) {
+        // Преобразование драйвера в объект TakesScreenshot
+        TakesScreenshot ts = (TakesScreenshot) BrowserManager.getDriver();
+        // Получение скриншота как файла
+        File screenshot = ts.getScreenshotAs(OutputType.FILE);
+        // Путь для сохранения скриншота
+        String path = "./screenshots/" + result.getName() + ".png";
+        // Создание файла скриншота
+        File destination = new File(path);
+        try {
+            // Копирование скриншота в нужное место
+            FileUtils.copyFile(screenshot, destination);
+            // Добавление скриншота к отчету TestNG
+            Reporter.log("Failed test screenshot: <a href='" + path + "'><img src='" + path + "'/></a>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
