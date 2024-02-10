518e57d84009ef9c6e335f11265b68a5e4d7f22f
Bakhmeteva Yulya <bakhmetevayulya@gmail.com>
2024-02-10 16:56:45 +0200

    Еще попытка скрина

--- src/test/java/study/alevel/BaseTest.java
+++ src/test/java/study/alevel/BaseTest.java
@@ -1,10 +1,13 @@
 package study.alevel;
 
+import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import study.alevel.core.BrowserManager;
+
+import static study.alevel.core.BrowserManager.driver;
 
 public class BaseTest {
 
@@ -21,6 +24,19 @@ public class BaseTest {
     @BeforeMethod
     public void goToBasePage(){
         BrowserManager.getInstance().open();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            // Сделать скриншот и сохранить его
+            takeScreenshot();
+        }
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) BrowserManager.getDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
 }
